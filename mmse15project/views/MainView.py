5f4bec086b1ba7d5a1ee8a09ff63c744593f3b53
Tobias Johansson <tobiajo@kth.se>
2015-10-13 00:54:48 +0200

    Minor change

--- mmse15project/views/MainView.py
+++ mmse15project/views/MainView.py
@@ -8,11 +8,12 @@ class MainView(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
         self.title("Mailman")
+        self.resizable(tk.FALSE, tk.FALSE)
         self.attributes("-topmost", True)
 
         self.container = tk.Frame(self)
         separator = ttk.Separator(self, orient=tk.HORIZONTAL)
-        company = tk.Label(self, text="© 2015 Swedish Events Planners (SEP)",
+        company = tk.Label(self, text="© 2015 United Swedish Solutions (USS)",
                            font="-slant italic")
 
         self.container.grid(row=0, column=0)
