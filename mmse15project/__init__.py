937e9edb55a93192bb2d4af80ce6f81c6bb29d47
Tobias Johansson <tobiajo@kth.se>
2015-10-14 15:19:37 +0200

    Remove unneeded import

--- mmse15project/__init__.py
+++ mmse15project/__init__.py
@@ -2,7 +2,6 @@ from mmse15project.model.Model import Model
 from mmse15project.views.MainView import MainView
 from mmse15project.ctrls.MainController import MainController
 from mmse15project.views.Login import Login
-from mmse15project.model.DBConnectionSQLite import  DBInterface
 
 
 def main():
