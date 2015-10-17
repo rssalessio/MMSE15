e2580b75435635bc725cc656bfbaff447fa14148
Tobias Johansson <tobiajo@kth.se>
2015-10-17 17:56:47 +0200

    Implement sticky

--- mmse15project/views/MainView.py
+++ mmse15project/views/MainView.py
@@ -10,11 +10,8 @@ class MainView(tk.Tk):
         self.resizable(tk.FALSE, tk.FALSE)
         self.attributes("-topmost", True)
 
-        #ttk_root = ttk.Frame(self)
-        #ttk_root.pack()
-
         self.container = ttk.Frame(self)
-        self.container.grid(row=0)
-        ttk.Separator(self, orient=tk.HORIZONTAL).grid(row=1, sticky="we")
+        self.container.grid(row=0, sticky="WE")
+        ttk.Separator(self, orient=tk.HORIZONTAL).grid(row=1, sticky="WE")
         ttk.Label(self, text="© 2015 United Swedish Solutions (USS)",
-                  font="-slant italic").grid(row=2)
+                  font="-slant italic").grid(row=2, sticky="WE")
